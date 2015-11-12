8b4ede5aff53aa4ea3962ce02918eb73849e7635
INGCRENGIFO <ingcrengifo@outlook.com>
2015-11-12 06:22:34 -0500

    Se Agregan menus
    Se agregan menus y se cambia nombre a escena del nivel uno como nivel1-1

--- Assets/Mis scrips/BotonMenu.cs
+++ Assets/Mis scrips/BotonMenu.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class BotonMenu : MonoBehaviour {
 
+	public GameObject pauseScreen;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -15,5 +17,28 @@ public class BotonMenu : MonoBehaviour {
 
 	public void Jugar(){
 		Application.LoadLevel(1); //se puede cambiar por el numero de la escena de file-builsetting 		
+	}
+
+	public void Salir(){
+		Application.Quit ();
+	}
+
+	public void Pausa()
+	{
+		Time.timeScale = 0;
+		
+		if (pauseScreen!=null) {
+			pauseScreen.SetActive (true);
+		}
+		
+	}
+	
+	public void UnPause()
+	{
+		Time.timeScale = 1;
+		
+		if (pauseScreen!=null) {
+			pauseScreen.SetActive (false);
+		}
 	}
 }
